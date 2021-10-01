8c866b6c3bb3e7ef257c494b715483f40ed9f044
kanhaiya-bhayana <kbahayan60_be20@thapar.edu>
2021-10-01 18:24:17 +0530

    initial commit

---
+++ init.js
@@ -0,0 +1,17 @@
+// fetch('https://icanhazdadjoke.com/slack')
+//     .then(data => data.json())
+//     .then(jokeData =>{
+//         const jokeText = jokeData.attachments[0].text;
+//         const jokeElement = document.getElementById('jokeElement');
+        
+//         jokeElement.innerHTML = '"' + jokeText + '"';
+//     })
+
+fetch("https://meme-api.herokuapp.com/gimme", {
+        "method": "GET",
+    })
+    .then(response => response.json().then(body => {
+        let img_url = body.preview.pop()
+        jokeElement.innerHTML = `<img id="jokeElement" height="100%" width="100%" src="${img_url}" />`
+    }))
+    .catch(err => console.error(err));
